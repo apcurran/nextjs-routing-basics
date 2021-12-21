055974deb96c3bcee46d8b746e1abc091db68451
Alex Curran <alexcurran.va@gmail.com>
2021-12-21 13:34:22 -0600

    Implement Link component for SPA-behavior

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,18 @@
+import { Fragment } from "react";
+import Link from "next/link"; // Special Link comp that keeps SPA-behavior without a server req
+
 function NewsPage() {
-    return <h1>The News Page</h1>;
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href={"/news/next-js-is-a-framework"}>NextJS is a Framework</Link>
+                </li>
+                <li>Something else</li>
+            </ul>
+        </Fragment>
+    ); 
 }
 
 export default NewsPage;
